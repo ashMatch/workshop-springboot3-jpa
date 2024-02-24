c0dc11c7c651add383166b6a02ca523626c756c2
ashMatch <bruno.rochah95@gmail.com>
2024-02-24 10:10:56 -0300

    create Service layer, component resgistration

---
+++ src/main/java/com/jellyfish/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.jellyfish.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jellyfish.course.entities.User;
+import com.jellyfish.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
