de4684ccdb30eefadd9eeba49132d0fde279e3ab
ashMatch <bruno.rochah95@gmail.com>
2024-02-25 19:54:04 -0300

    create Payment class, and add one-to-one association

--- src/main/java/com/jellyfish/course/config/TestConfig.java
+++ src/main/java/com/jellyfish/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jellyfish.course.entities.Category;
 import com.jellyfish.course.entities.Order;
 import com.jellyfish.course.entities.OrderItem;
+import com.jellyfish.course.entities.Payment;
 import com.jellyfish.course.entities.Product;
 import com.jellyfish.course.entities.User;
 import com.jellyfish.course.entities.enums.OrderStatus;
@@ -71,9 +72,9 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
 		
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.SHIPPED, u1);	
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID , u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.SHIPPED, u1);	
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
@@ -88,6 +89,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	
 	
